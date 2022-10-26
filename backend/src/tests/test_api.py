95975df3bcc95b076ea0d4c2f07fefa5d038048d
willidert <williderteixeira@gmail.com>
2022-10-26 11:03:50 -0400

    test: list and create categories

--- backend/src/tests/test_api.py
+++ backend/src/tests/test_api.py
@@ -1,5 +1,31 @@
 from fastapi.testclient import TestClient
-from main import app
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from ..main import app, get_db
+
+from ..database import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
@@ -10,3 +36,25 @@ def test_get_products():
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     assert len(response.json()) == 0
+
+
+def test_get_categories():
+    response = client.get('/categories')
+
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+    assert len(response.json()) == 0
+
+
+def test_create_category():
+    CATEGORY = {
+        "name": "A"
+    }
+
+    response = client.post("/categories", json=CATEGORY)
+
+    assert response.status_code == 200
+
+    data = response.json()
+    assert "name" in data
+    assert CATEGORY["name"] == data["name"]
