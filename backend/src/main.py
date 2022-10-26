121bfa53f9fddb679322810bf1c291f473b75d2f
willidert <williderteixeira@gmail.com>
2022-10-26 13:23:53 -0400

    feat: get, update and delete product

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from .database import SessionLocal, engine
 
 from sqlalchemy.orm import Session
@@ -20,12 +20,6 @@ def get_db():
         db.close()
 
 
-@app.get("/products", response_model=List[schemas.Product])
-def list_products(db: Session = Depends(get_db)):
-    products = service.get_products(db)
-    return products
-
-
 @app.get("/categories", response_model=List[schemas.Category])
 def list_categories(db: Session = Depends(get_db)):
     categories = service.get_categories(db)
@@ -37,6 +31,27 @@ def create_category(category: schemas.CategoryCreate, db: Session = Depends(get_
     return service.create_category(db, category)
 
 
+@app.get("/products", response_model=List[schemas.Product])
+def list_products(db: Session = Depends(get_db)):
+    products = service.get_products(db)
+    return products
+
+
 @app.post("/products", response_model=schemas.Product)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
     return service.create_product(db, product)
+
+
+@app.get("/products/{product_id}", response_model=Optional[schemas.Product])
+def get_product_by_id(product_id: int, db: Session = Depends(get_db)):
+    return service.get_product_by_id(db, product_id)
+
+
+@app.patch("/products/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_product(product_id: int, product: schemas.ProductUpdate, db: Session = Depends(get_db)):
+    return service.update_product(db, product, product_id)
+
+
+@app.delete("/products/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    return service.delete_product(db, product_id)
