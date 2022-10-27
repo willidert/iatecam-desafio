f1ad3b2e1e95b283e2d6aea4eca06480c9924ecd
willidert <williderteixeira@gmail.com>
2022-10-27 09:13:11 -0400

    ci: remove unnecessary flag

--- backend/src/database.py
+++ backend/src/database.py
@@ -12,18 +12,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-PYTHON_MODE = os.getenv('PYTHON_MODE')
-
-if PYTHON_MODE == "dev":  # flag para o ci
-    SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL
-    )
-else:
-    SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL_CI')
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
