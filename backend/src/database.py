afb0307d3d67939ad333c89927f1fe460616dceb
willidert <williderteixeira@gmail.com>
2022-10-27 08:44:35 -0400

    ci: configure database urls

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,19 +1,29 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-SQLALCHEMY_DATABASE_URL = "postgresql://iatecam:password@localhost/iatecam"
 
 # MYSQL_DATABASE_URL = "mysql+pymsql://iatecam:password@127.0.0.1/iatecam"
 
 # MARIADB_DATABASE_URL = "mysql://will:password@localhost/test"
 
-# retirar esse check same thread
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+load_dotenv()
+
+PYTHON_MODE = os.getenv('PYTHON_MODE')
+
+if PYTHON_MODE == "dev":  # flag para o ci
+    SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL
+    )
+else:
+    SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL_CI')
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
