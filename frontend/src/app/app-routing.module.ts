0e3b0d2aa1d442350d953e7a61c4e8b63a45a797
willidert <williderteixeira@gmail.com>
2022-10-28 20:57:52 -0400

    feat: add categories module routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,6 +8,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./products/products.module').then((m) => m.ProductsModule),
   },
+  {
+    path: 'categories',
+    loadChildren: () =>
+      import('./categories/categories.module').then((m) => m.CategoriesModule),
+  },
 ];
 
 @NgModule({
