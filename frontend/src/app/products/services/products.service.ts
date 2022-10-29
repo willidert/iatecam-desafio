332633e0cfd9ce05d69a62f170fc39cfa8b5879f
willidert <williderteixeira@gmail.com>
2022-10-28 20:48:59 -0400

    feat: save product and correcting url

--- frontend/src/app/products/services/products.service.ts
+++ frontend/src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { first, Observable } from 'rxjs';
 
 import { Product } from '../model/product';
 
@@ -11,10 +11,14 @@ export class ProductsService {
   readonly API_URL: string;
 
   constructor(private client: HttpClient) {
-    this.API_URL = 'http://localhost:8000';
+    this.API_URL = 'http://localhost:8000/products';
   }
 
   list(): Observable<Product[]> {
-    return this.client.get<Product[]>(`${this.API_URL}/products`);
+    return this.client.get<Product[]>(this.API_URL);
+  }
+
+  save(product: Partial<Product>): Observable<Product> {
+    return this.client.post<Product>(this.API_URL, product);
   }
 }
