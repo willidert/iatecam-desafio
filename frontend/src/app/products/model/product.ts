cc866deeb36ac166cd367a978266c4ee247c260e
willidert <williderteixeira@gmail.com>
2022-10-30 09:08:22 -0400

    feat: update product interface

--- frontend/src/app/products/model/product.ts
+++ frontend/src/app/products/model/product.ts
@@ -1,7 +1,9 @@
+import { Category } from './../../categories/model/category';
 export interface Product {
   id: string;
   name: string;
   serie: number;
   price: number;
   category_id: number;
+  category?: Category;
 }
