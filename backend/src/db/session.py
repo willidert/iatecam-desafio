b462eadc53837e045cc86edb33de63b82cc71188
William Teixeira <williderteixeira@gmail.com>
2022-10-30 03:14:17 -0300

    Feat backend migrations (#10)
    * feat: add alembic configurations
    
    * refactor: separate database session and angine
    
    * refactor: separate models
    
    * feat: add migrations and migration.sql
    
    * refator: update imports and update schemas
    
    * refactor: update dockerfiles for alembic migration
    
    * chore: add news instructions for execute project
    
    * chore: update table
    
    * tests: update imports (fix CI)

--- backend/src/db/session.py
+++ backend/src/db/session.py
@@ -1,14 +1,8 @@
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
-# MYSQL_DATABASE_URL = "mysql+pymsql://iatecam:password@127.0.0.1/iatecam"
-
-# MARIADB_DATABASE_URL = "mysql://will:password@localhost/test"
 
 load_dotenv()
 
@@ -19,10 +13,6 @@ SERVER = os.getenv('POSTGRES_SERVER')
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{USER}:{PASSWORD}@{SERVER}/{DB}'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
